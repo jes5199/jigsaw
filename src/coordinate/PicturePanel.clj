832107a2781f897adc899bad9505ef5b43cef5e6
Jesse Wolfe <jes5199@gmail.com>
2013-05-29 22:07:05 -0700

    capable of loading a jpeg at all

--- src/coordinate/PicturePanel.clj
+++ src/coordinate/PicturePanel.clj
@@ -2,8 +2,13 @@
   (:gen-class
     :extends javax.swing.JPanel
     :name coordinate.PicturePanel
-    :exposes-methods {paintComponent parentPaintComponent}))
+    :exposes-methods {paintComponent parentPaintComponent})
+  (:import (java.awt Toolkit)))
 
 (defn -paintComponent [this g]
   (.parentPaintComponent this g)
+  (let [
+      toolkit (Toolkit/getDefaultToolkit)
+      image (.getImage toolkit "test.jpg") ]
+    (.drawImage g image 0 0 (.getWidth this) (.getHeight this) nil ))
   (.drawOval g 0 0 (.getWidth this) (.getHeight this)))
